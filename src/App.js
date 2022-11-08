3500020bcfbf9d3ba3a8454279c16aa907ff2d27
Daniel Chiang <generalyi0057@gmail.com>
2022-11-08 16:49:49 -0600

    Revert "Local weather"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import WeatherBoard from "./weather/WeatherBoard";
-
-import './App.sass';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <WeatherBoard />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
